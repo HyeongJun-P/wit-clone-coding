e40dbfa8b5c84d70020c6d20012ea8f520833065
765prod <80656271+765prod@users.noreply.github.com>
2021-04-15 17:35:08 +0900

    firebase-id-value
    yes

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,11 @@
 import firebase from "firebase/app";
 const firebaseConfig = {
-    apiKey: "AIzaSyA6g5v_5bIQ4A-a30jW-bMun-1wVGzc_AE",
-    authDomain: "witter-7b92c.firebaseapp.com",
-    projectId: "witter-7b92c",
-    storageBucket: "witter-7b92c.appspot.com",
-    messagingSenderId: "519258488268",
-    appId: "1:519258488268:web:6c4afd4ef2999300a47169"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+    appId: process.env.EACT_APP_APP_ID
   };
   // Initialize Firebase
   export default firebase.initializeApp(firebaseConfig);
