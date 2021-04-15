a22d5837e4cdb6c4b6499a6ee735b534a7831b25
Your Name <you@example.com>
2021-04-15 13:38:03 +0900

    master

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase/app";
+const firebaseConfig = {
+    apiKey: "AIzaSyA6g5v_5bIQ4A-a30jW-bMun-1wVGzc_AE",
+    authDomain: "witter-7b92c.firebaseapp.com",
+    projectId: "witter-7b92c",
+    storageBucket: "witter-7b92c.appspot.com",
+    messagingSenderId: "519258488268",
+    appId: "1:519258488268:web:6c4afd4ef2999300a47169"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
