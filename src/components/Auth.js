96852ec07d2e752277256631d7748c99daf0c132
765prod <80656271+765prod@users.noreply.github.com>
2021-04-15 17:41:31 +0900

    compo

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,3 +1,3 @@
 import React, {Component} from 'react';
 
-export default <span>Auth</span>;
+export default () => <span>Auth</span>;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,3 @@
 import React, {Component} from 'react';
 
-export default <span>Home</span>;
+export default () => <span>Home</span>;
